5ec7a5c510993abcf156851124f32539c6d47c49
haruki33 <kkhk0869hs33@gmail.com>
2026-05-20 17:39:53 +0900

    従業員情報を全て変更できるようなフォームに変更

--- src/main/java/com/example/form/UpdateEmployeeForm.java
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -2,6 +2,8 @@ package com.example.form;
 
 import lombok.*;
 
+import java.sql.Date;
+
 /**
  * 従業員の扶養人数変更時に使用するフォーム.
  */
@@ -13,9 +15,59 @@ import lombok.*;
 @Builder
 public class UpdateEmployeeForm {
     /**
-     * id
+     * ID
      */
     private String id;
+
+    /**
+     * 名前
+     */
+    private String name;
+
+    /**
+     * 画像
+     */
+    private String image;
+
+    /**
+     * 性別
+     */
+    private String gender;
+
+    /**
+     * 入社日
+     */
+    private Date hireDate;
+
+    /**
+     * メールアドレス
+     */
+    private String mailAddress;
+
+    /**
+     * 郵便番号
+     */
+    private String zipCode;
+
+    /**
+     * 住所
+     */
+    private String address;
+
+    /**
+     * 電話番号
+     */
+    private String telephone;
+
+    /**
+     * 給料
+     */
+    private String salary;
+
+    /**
+     * 特性
+     */
+    private String characteristics;
 
     /**
      * 扶養人数
