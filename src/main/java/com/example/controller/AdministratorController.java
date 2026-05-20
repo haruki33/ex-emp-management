bb93205fee43b0e2cc42c1fbceed4a00debd436e
haruki33 <kkhk0869hs33@gmail.com>
2026-05-20 13:37:57 +0900

    管理者登録機能を実装
    AdministratorServiceにinsertメソッドを追加
    
    insertメソッドの追加
    
    javadocの追記

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -26,5 +29,20 @@ public class AdministratorController {
     @GetMapping("/to-insert")
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
+    }
+
+    /**
+     * 管理者登録（insert）を行う.
+     *
+     * @param form 管理者登録に使うフォーム
+     * @return ログイン画面にリダイレクト
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        ModelMapper modelMapper = new ModelMapper();
+        Administrator administrator = modelMapper.map(form, Administrator.class);
+        service.insert(administrator);
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,4 +15,13 @@ public class AdministratorService {
 
     @Autowired
     private AdministratorRepository repository;
+
+    /**
+     * 管理者情報を挿入する.
+     *
+     * @param administrator 管理者情報
+     */
+    public void insert(Administrator administrator) {
+        repository.insert(administrator);
+    }
 }
